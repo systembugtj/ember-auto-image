d6bfae56eddaf6769ea4f7ec04f54b91a28558b1
systembugtj <albert_lee@hotmail.com>
2019-09-16 17:19:02 -0700

    fix plugin included hook

--- index.js
+++ index.js
@@ -3,9 +3,15 @@
 
 module.exports = {
   name: require('./package').name,
-
-  included: function(app) {
-    app.import('./vendor/aspect-ratio-image.css');
+  included(app, parentAddon) {
+    this._super.included.apply(this, arguments);
+    let target = (parentAddon || app);
+    // Now you can modify the app / parentAddon. For example, if you wanted
+    // to include a custom preprocessor, you could add it to the target's
+    // registry:
+    //
+    //     target.registry.add('js', myPreprocessor);
+    target.import('./vendor/aspect-ratio-image.css');
   }
 
 };
